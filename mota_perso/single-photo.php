8fb7002ca397f100425065d0ce1b5d02091ccd39
Audrey Picarel <aud.picarel@gmail.com>
2026-01-31 19:46:02 +0100

    mise en place du showcase des photos en proposant par même catégorie _via php

--- mota_perso/single-photo.php
+++ mota_perso/single-photo.php
@@ -55,15 +55,33 @@ get_header();
             </span>
         </div>
         <div class="preview_next">
-        <?php 
-            // Récupérer la référence actuelle du CPT pour la navigation avec les flèches via JS et AJAX
-            $current_photo_url = get_the_post_thumbnail_url( get_the_ID(), 'full' ); //get_the_post_thumbnail_url récupère l'URL de l'image mise en avant sans l'afficher
-        ?>
-            <img src="" alt="Aperçu de la photo suivante" class="preview_image">
+            <?php // Récupération des posts précédent et suivant via les fonctions WordPress
+            $prev_post = get_previous_post();
+            $next_post = get_next_post();
+            ?>
+
+            <?php if ( !empty($next_post) ) : 
+                // On récupère l'URL de l'image suivante
+                $next_thumb_url = get_the_post_thumbnail_url( $next_post->ID, 'thumbnail' );
+            ?>
+                <a href="<?php echo get_permalink($next_post->ID); ?>" class="nav-link-next">
+                    <img src="<?php echo $next_thumb_url; ?>" alt="Suivant" class="preview_image">
+                </a>
+            <?php endif; ?>
+
             <div class="arrows_navigation">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/left_arrow.svg" alt="Précédente" class="preview_arrow">                         
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/right_arrow.svg" alt="Suivante" class="next_arrow">      
-            </div> 
+                <?php if ( !empty($prev_post) ): ?>
+                    <a href="<?php echo get_permalink($prev_post->ID); ?>">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/left_arrow.svg" alt="Précédente" class="preview_arrow">                         
+                    </a>
+                <?php endif; ?>
+
+                <?php if ( !empty($next_post) ): ?>
+                    <a href="<?php echo get_permalink($next_post->ID); ?>">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/right_arrow.svg" alt="Suivante" class="next_arrow">      
+                    </a>
+                <?php endif; ?>
+            </div>
         </div>
     </div>
     <p><?php the_content(); ?></p>
@@ -71,11 +89,87 @@ get_header();
     <div class="other_photos_section">
         <h3>Vous aimerez aussi </h2>
         <div class="other_photos_container">
-            <a href="<?php //lien vers la photo 1 ?>" class="other_photo_item">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/nathalie-1.jpeg" alt="Photo 1">
+            <?php 
+            // Récupération des posts précédent et suivant dans la même catégorie dans des variables
+            $next_post_showcase = get_next_post( true, '', 'categorie' );
+            $prev_post_showcase = get_previous_post( true, '', 'categorie' );
+
+            //Déclaration des derniers recours possibles en bouclant les posts
+                // Le tout premier post (le plus ancien)
+                //1. récupérer le premier post publié = au 1er en partant de l'ordre croissant
+                $first_post_query = get_posts(array('post_type' => 'photo', 'posts_per_page' => 1, 'order' => 'ASC'));
+                //2. vérifier qu'on a bien un post et l'assigner à la variable [le n° 0 du tableau renvoyé par get_posts, CAD le 1er du début]
+                if(!empty($first_post_query)) {
+                    $absolute_first = $first_post_query[0];
+                } else {
+                    $absolute_first = null;
+                }
+
+                // Le tout dernier post (le plus récent)
+                //1. récupérer le dernier post publié = au 1er en partant de l'ordre décroissant
+                $last_post_query = get_posts(array('post_type' => 'photo', 'posts_per_page' => 1, 'order' => 'DESC'));
+                //2. vérifier qu'on a bien un post et l'assigner à la variable [le n° 0 du tableau renvoyé par get_posts, CAD le 1er de la fin]
+                if(!empty($last_post_query)){
+                    $absolute_last = $last_post_query[0];
+                } else {
+                    $absolute_last = null ;
+                }
+
+            //Attribution du post à la variable du post suivant en testant les différents cas possibles
+                    //Si pas de post dans la même catégorie, on cherche dans la taxonomie année
+            if (empty($next_post_showcase)){
+                $next_post_showcase = get_next_post( true, '', 'annee' );
+            }
+                    //Si pas de post dans la même année, on cherche dans la taxonomie format
+            if (empty($next_post_showcase)){
+                $next_post_showcase = get_next_post( true, '', 'format' );
+            }
+                    //Si pas de post dans la même format non plus, on cherche dans le post suivant sans filtre
+            if (empty($next_post_showcase)){
+                $next_post_showcase = get_next_post( false );
+            }
+                    //Si pas de post suivant, on cherche dans le post précédent sans filtre
+            if (empty($next_post_showcase)){
+                $first_posts = get_posts(array(
+                    'post_type'      => 'photo',
+                    'posts_per_page' => 1,
+                    'order'          => 'ASC'
+                ));
+                if (!empty($first_posts)) {
+                    $next_post_showcase = $absolute_first;
+                }
+            }
+
+            //Même chose pour le post précédent
+            if (empty($prev_post_showcase)){
+                $prev_post_showcase = get_previous_post( true, '', 'annee' );
+            }
+            if (empty($prev_post_showcase)){
+                $prev_post_showcase = get_previous_post( true, '', 'format' );
+            }
+            if (empty($prev_post_showcase)){
+                $prev_post_showcase = get_previous_post( false );
+            }
+            if (empty($prev_post_showcase)){
+                $prev_post_showcase = $absolute_last;
+            }
+
+            //Attribution des liens et des images aux variables
+            if ( !empty($next_post_showcase) ) {
+                $next_url_showcase = get_permalink($next_post_showcase->ID);
+                $next_thumb_showcase = get_the_post_thumbnail_url($next_post_showcase->ID, 'full');
+            }
+            if ( !empty($prev_post_showcase) ) {
+                $prev_url_showcase = get_permalink($prev_post_showcase->ID);
+                $prev_thumb_showcase = get_the_post_thumbnail_url($prev_post_showcase->ID, 'full');
+            }
+            //Affichage des deux photos
+            ?>
+            <a href="<?php echo $prev_url_showcase; ?>" class="other_photo_item">
+                <img src="<?php echo $prev_thumb_showcase; ?>" alt="Photo 1">
             </a>
-            <a href="<?php //lien vers la photo 2 ?>" class="other_photo_item">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/nathalie-2.jpeg" alt="Photo 2">
+            <a href="<?php echo $next_url_showcase; ?>" class="other_photo_item">
+                <img src="<?php echo $next_thumb_showcase;?>" alt="Photo 2">
             </a>
         </div>
     </div>
